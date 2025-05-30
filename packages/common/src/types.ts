4092efc821a3896f5e57c8f6e8c9befee6f9a579
Ansh-699 <anshtyagi7845@gmail.com>
2025-05-30 23:27:21 +0530

    schema-fix

--- packages/common/src/types.ts
+++ packages/common/src/types.ts
@@ -9,7 +9,7 @@ export const CreateUserSchema = z.object({
 
 
 export const SigninSchema = z.object({
-    username: z.string().min(3).max(20),
+    email: z.string().min(3).max(20),
     password: z.string().min(6).max(100),
 });
 
